f042160957d9c18a82f826f4eee1711ac5952be0
David <goodescientist@gmail.com>
2019-03-18 11:01:23 -0400

    removed method

--- David_JavaChallenge/JavaChallenge/src/main/java/com/javachallenge/Week4Challenge.java
+++ David_JavaChallenge/JavaChallenge/src/main/java/com/javachallenge/Week4Challenge.java
@@ -1,6 +1,8 @@
 package com.javachallenge;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Hello world!
@@ -8,18 +10,29 @@ import java.util.ArrayList;
  */
 public class Week4Challenge {
 
+    // public static boolean isPalindrome(String str) {
+    // return str.equals(new StringBuilder(str).reverse().toString());
+    // }
+
     public static boolean isPalindrome(String str) {
-        return str.equals(new StringBuilder(str).reverse().toString());
+        int n = str.length();
+        for (int i = 0; i < n; i++)
+            if (str.charAt(i) != str.charAt(n - i - 1))
+                return false;
+        return true;
     }
 
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
+        List<String> arrayList;
         ArrayList<String> palindrome = new ArrayList<>();
 
         String[] strArray = { "karan", "madam", "tom", "civic", "radar", "sexes", "jimmy", "kayak", "john", "refer",
                 "billy", "did" };
 
-        for (String s : strArray) {
+        arrayList = Arrays.asList(strArray);
+
+        for (String s : arrayList) {
             if (isPalindrome(s)) {
                 palindrome.add(s);
             } else
@@ -27,6 +40,7 @@ public class Week4Challenge {
         }
         System.out.println("Regular Words: " + list);
         System.out.println("Palindromes: " + palindrome);
-        
+        //System.out.println(arrayList);
+
     }
 }
